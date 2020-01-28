658b883d4eb6f3478bfb731bea741eb596f29fec
valteir <valteir.ea@hotmail.com>
2020-01-27 22:26:06 -0300

    Aula 05

--- src/main/java/com/matera/cursoferias/digitalbank/AppStartupRunner.java
+++ src/main/java/com/matera/cursoferias/digitalbank/AppStartupRunner.java
@@ -1,47 +1,60 @@
 package com.matera.cursoferias.digitalbank;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.matera.cursoferias.digitalbank.dto.request.ClienteRequestDTO;
 import com.matera.cursoferias.digitalbank.entity.Cliente;
-import com.matera.cursoferias.digitalbank.repository.ClienteRepository;
+import com.matera.cursoferias.digitalbank.service.ClienteService;
 
 @Component
 public class AppStartupRunner implements ApplicationRunner {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+	private final ClienteService clienteService;
+	
+		
+    public AppStartupRunner(ClienteService clienteService) {
+		this.clienteService = clienteService;
+	}
 
-    @Override
+
+
+	@Override
     public void run(ApplicationArguments args) throws Exception {
-        Cliente cliente = new Cliente("Cliente 1",
-                                      "01234567890",
-                                      4499998877L,
-                                      BigDecimal.valueOf(1000),
-                                      "Rua Santos Dumont",
-                                      123,
-                                      null,
-                                      "Centro",
-                                      "Maringá",
-                                      "PR",
-                                      "87087087");
+    	ClienteRequestDTO cliente =  ClienteRequestDTO.builder().bairro("bairro")
+    	.cep("6546546")
+    	.cidade("Maringá")
+    	.complemento(null)
+    	.cpf("8702351055")
+    	.estado("pr")
+    	.logradouro("teste")
+    	.nome("Valteir")
+    	.numero(123)
+    	.rendaMensal(BigDecimal.valueOf(5000l))
+    	.telefone(998609170l)
+    	.build();
+    	
+    	
+    	ClienteRequestDTO cliente2 =  ClienteRequestDTO.builder().bairro("bairro")
+    	    	.cep("6546546")
+    	    	.cidade("Maringá")
+    	    	.complemento(null)
+    	    	.cpf("8702356055")
+    	    	.estado("pr")
+    	    	.logradouro("teste")
+    	    	.nome("Valteir")
+    	    	.numero(123)
+    	    	.rendaMensal(BigDecimal.valueOf(5000l))
+    	    	.telefone(998609140l)
+    	    	.build();
+    	
+    	clienteService.cadastra(cliente);  
+    	clienteService.cadastra(cliente2);
+    	
 
-        clienteRepository.save(cliente);
-
-        Cliente cliente2 = clienteRepository.findById(cliente.getId()).orElse(null);
-        System.out.println("Cliente 2: " + cliente2);
-
-        Cliente cliente3 = clienteRepository.findByCpf(cliente.getCpf()).orElse(null);
-        System.out.println("Cliente 3: " + cliente3);
-
-        clienteRepository.delete(cliente2);
-        Optional<Cliente> cliente6 = clienteRepository.findById(cliente.getId());
-        System.out.println(cliente6.isPresent());
     }
 
 }
--- src/main/java/com/matera/cursoferias/digitalbank/dto/response/ContaResponseDTO.java
+++ src/main/java/com/matera/cursoferias/digitalbank/dto/response/ContaResponseDTO.java
@@ -2,6 +2,9 @@ package com.matera.cursoferias.digitalbank.dto.response;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +16,8 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class ContaResponseDTO {
-
     private Long idCliente;
     private Long idConta;
     private Integer numeroAgencia;
